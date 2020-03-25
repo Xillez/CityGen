6e378b9db9e6d61c3d7e6e62dea60d2e9ebc3a1b
Xillez <kent.wincent46@gmail.com>
2020-03-25 17:42:08 +0100

    Implemmented city object

--- Assets/Scripts/CityGen/City.cs
+++ Assets/Scripts/CityGen/City.cs
@@ -1,16 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
+[ExecuteInEditMode]
 public class City : MonoBehaviour
 {
-	void Start ()
-	{
-		
-	}
-	
-	void Update ()
-	{
-		
-	}
+    [SerializeField] private string cityName {get; set;}
+    [SerializeField] public ScriptableObject generator;
+    [SerializeField] private List<RoadSegment> roadSegments;
+
+    public void Setup()
+    {
+        this.cityName = this.transform.gameObject.name;
+
+        if (this.roadSegments == null)
+            this.roadSegments = new List<RoadSegment>();
+    }
+
+    public List<RoadSegment> GetSegments(RoadSegment exclude = null)
+    {
+        if (exclude == null || this.roadSegments == null)    // Nothing to exclude, give all
+            return this.roadSegments;
+
+        List<RoadSegment> temp = new List<RoadSegment>(this.roadSegments); 
+        temp.RemoveAt(this.roadSegments.IndexOf(exclude));
+        return temp;
+    }
+
+    public RoadSegment GetSegment(int index)
+    {
+        if (this.roadSegments == null)
+            return null;
+
+        return this.roadSegments[index];
+    } 
+
+    public void AddSegment(RoadSegment segment)
+    {
+        if (this.roadSegments != null)
+            this.roadSegments.Add(segment);
+    }
+
+    public void RemoveSegment(int index)
+    {
+        if (this.roadSegments != null)
+            this.roadSegments.RemoveAt(index);
+    }
+
+    public void RemoveSegment(RoadSegment segment)
+    {
+        if (this.roadSegments != null)
+            this.roadSegments.Remove(segment);
+    }
 }
